ac81a77534f1256f40c9a15416582263ab08ca4a
Michael Pfeuti (U121978) <michael.pfeuti@mobi.ch>
2026-05-13 20:01:45 +0200

    implemented new json export feature

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import json
+
 print("Hallo Welt")
 
 print("asdlkjnakjscnHallo Unterwelt!!")
+
+some_data = {"password": "123",
+             "username": "admin",
+             "email": "admin@admin.ch"}
+
+json.dumps(some_data)
